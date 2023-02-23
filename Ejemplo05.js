22e30f78612e7aae26e8ec9516e4d1e9c7fa1ee1
kodigo-denny <denny.herrera@kodigo.org>
2023-02-22 20:14:30 -0600

    Cambios 22/02/2023

--- Ejemplo05.js
+++ Ejemplo05.js
@@ -10,38 +10,46 @@ class Document{
     open(){
         console.log(`Archivo ${this.filename} aperturado`)
     }
-
-    save(){
-        console.log(`Archivo ${this.filename} almacenado`)
-    }
 }
 
-class ReadOnlyDocument extends Document{
+
+class WritableDocument extends Document{
 
     constructor(data, filename){
         super(data, filename)
     }
 
     save(){
-        alert("Error es un archivo de solo lectura")
+        console.log(`Archivo ${this.filename} almacenado`)
     }
 }
 
 class Project{
-    documents
+    allDocs
+    writableDocs
 
     constructor(){
-        this.documents = []
+        this.allDocs = []
+        this.writableDocs = []
+    }
+
+    agregarSoloLectura(doc){
+        this.allDocs.push(doc)
+    }
+
+    agregarArchivo(doc){
+        this.agregarSoloLectura(doc)
+        this.writableDocs.push(doc)
     }
 
     openAll(){
-        for(let item of this.documents){
+        for(let item of this.allDocs){
             item.open()
         }
     }
 
     saveAll(){
-        for(let item of this.documents){
+        for(let item of this.writableDocs){
             item.save()
         }
     }
